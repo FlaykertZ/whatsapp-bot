c769009a9139da2f54a5b153bc1a2008c377e3d2
Flaykert <MasterGaming9999@gmail.com>
2025-05-30 19:49:33 +0300

    Inital commit

---
+++ replyMessages.js
@@ -0,0 +1,82 @@
+function mainMessage(message){
+    message.reply(`היי וברוכה הבאים למערך תקשוב חטיבה 5\n
+        אנו נותנים מענה לכלל משתמשי המחשוב בחטיבה\n
+        בשעות הפעילות שלנו נעות בין השעות 00:00 ל00:00 ליום שאחרי.\n
+
+        לתקלות בנושא מחשוב, יש לרשום 1
+        לתקלות בנושאים אחרים, יש לרשום 2
+        לרעיונות לבוט, הוספה, באגים וכו, יש לרשום 3
+        \n
+        בוט זה נוצר על ידי ד. חבקין\n
+        שימו לב שבוט זה עדיין בפיתוח וישנן פונקציות שלא יעבדו 100 אחוז`)
+}
+
+function computerProblem(message){
+    message.reply(`לתקלות בנושא ווינדואוס, יש לרשום 1
+        לתקלות בנושא טלפונים, יש לרשום 2
+         לתקלות בנושא אינטרנט, יש לרשום 3
+        `)
+}
+
+function otherProblems(message){
+    message.reply(`לתקלות בנושא משואה, יש לרשום 1
+        למספרי טלפון חשובים, יש לרשום 2
+        לתקלות בנושא כרטיסים, יש לרשום 3
+        `)
+}
+
+function windowsProblems(message){
+    message.reply(`היי, לפניך מספר פתרונות לתקלות שיכולות להיות לך בווינדואוס
+        במידה והפתרונות האלו לא עוזרות, יש לרשום "היי" ולבקש נציג
+        \n
+        במידה והמחשב תקוע, דברים עולים לאט וכו*, יש לבצע כיבוי והדלקה מלא למחשב*
+        שימו לב שיכול להיות משתמשים אחרים שמחוברים למחשב הזה וזה מה שגורם לו להיות איטי, במידה ואתם יודעים שיש מישהו שהיה מחובר למחשבכם ולא שמר דברים שהוא עבד עליהם
+        יש לבקש ממנו לשמור.
+        \n
+        `)
+}
+
+function telephoneProblems(message){
+    message.reply(`היי, לפניך מספר פתרונות לתקלות שיכולות להיות לך הקשורות לטלפוניה
+        במידה והפתרונות האלו לא עוזרות, יש לרשום "היי" ולבקש נציג
+        \n
+        במידה ואתם רוצים לבצע שיחות בטלפון *סיסקו*, אך אתם לא מקבלים שיחות או לא יכולים לשלוח שיחות
+        יש לוודא שהמשתמש מוגדר בסיסקו כמו שצריך
+        בשביל לעשות זאת יש להסתכל על המסך של הטלפון ולראות שרשום לכם "התחברות נציג"
+        במידה וכן, יש לרשום שם משתמש וסיסמא כאשר שם המשתמש והסיסמא זה מספר הטלפון של הסיסקו
+        *שימו לב*
+        כאשר אתם רושמים את המספר טלפון בשם המשתמש, יש לראות שזה רושם לכם מספרים ולא אותיות
+        במידה וזה רושם לכם אותיות יש ללחוץ כמה פעמים על אותו המספר עד שתראו את המספר עצמו.`)
+}
+
+function internetProblems(message){
+    message.reply(`היי, לפניך מספר פתרונות לתקלות שיכולות להיות לך הקשורות לאינטרנט
+        במידה והפתרונות האלו לא עוזרות, יש לרשום "היי" ולבקש נציג.
+        \n
+        במידה ואתם מחוברים למשתמש שלכם במחשב, אך רשום לכם שאין אינטרנט כאשר אתם פותחים כרום,
+        במידה ואתם אנשי מחשוב, יש לבדוק הגדרות רשת תואמות ושיש ביטים לאינטרנט
+        במידה ואתם לא אנשי מחשוב, יש לפנות לאנשי המחשוב שלכם.
+        \n
+        במידה ואתם לא מצליחים להתחבר למחשב שלכם, ורשום הודעה כגון "Domain specified is not available", יש לפנות לאנשי המחשוב
+        במידה ואתה איש מחשוב, יש להיכנס עם המשתמש החזק שלכם (לרוב .\\su) ולוודא הגדרות רשת.
+        במידה ואין לכם משתמש חזק, יש להיכנס דרך משתמש אדמין וסיסמאת לאפס, במידה ואין לכם סיסמאת לאפס או המחשב אינו בדומיין, יש לפרמט את המחשב.
+        \n`)
+}
+
+async function ideasMessage(client, message){
+    message.reply(`היי, אתם מוזמנים לרשום פה את הרעיון שלכם ונציג יתפנה אליכם בזמן הקרוב 🙏`)
+
+    const danielNumber = 972526872419
+    const chatId = danielNumber + "@c.us"
+
+    const danielMessage = `נציג מחפש אותך בבוט`
+
+    await client.sendMessage(chatId, danielMessage)
+}
+
+function workInProgress(message){
+    message.reply(`היי, מכיוון שבוט זה הוא חדש אין לו הרבה פונקציות, נשמח שתרשמו רעיונות ונוסיף אותם לבוט 🙏
+        \n בתודה, מנהל הבוט.`)
+}
+
+module.exports = { mainMessage, workInProgress, ideasMessage, computerProblem, otherProblems, windowsProblems, telephoneProblems, internetProblems };
---
+++ whatsappBot.js
@@ -0,0 +1,79 @@
+const { mainMessage, ideasMessage, workInProgress, computerProblem, otherProblems, windowsProblems, telephoneProblems, internetProblems } = require('./replyMessages'); // No .js needed in CommonJS
+
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+// Initialize client
+
+const client = new Client({
+    authStrategy: new LocalAuth() // 👈 Persistent login
+});
+
+// Show QR code in terminal for authentication
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+const greetedUsers = new Set();
+const answerOne = new Set();
+const answerTwo = new Set();
+
+// On successful login
+client.on('ready', () => {
+    console.log("Bot is ready!");
+});
+
+client.on('message', async (message) => {
+
+    const sender = message.from;
+    const isGroup = message.from.endsWith('@g.us');
+
+    if (isGroup) return;
+
+    if (message.body === 'היי' && greetedUsers.has(sender)) {
+
+        mainMessage(message)
+        answerTwo.delete(sender)
+        answerOne.add(sender)
+
+    }
+
+
+    if (!greetedUsers.has(sender)){
+        greetedUsers.add(sender);
+
+        mainMessage(message)
+
+        answerOne.add(sender)
+    }
+
+    if (message.body === '1' && answerOne.has(sender)) {
+
+        computerProblem(message)
+        answerTwo.add(sender)
+        answerOne.delete(sender)
+        
+    } else if (message.body === '2' && answerOne.has(sender)){
+
+        workInProgress(message)
+        answerOne.delete(sender)
+
+        //message.reply("הודעה זו אמירה להביא לכם עזרה לגבי תקלות שהם לוו דווקא מחשוב כגון עזרה בחיים, כלכלה וכו \n בוט זה עדיין בעבודה ולכן עצמכם הסליחה 🙏🙏")
+    } else if (message.body === '3' && answerOne.has(sender)) {
+        ideasMessage(client, message)
+    } else if (message.body === '1' && answerTwo.has(sender)){
+        windowsProblems(message)
+    } else if (message.body === '2' && answerTwo.has(sender)){
+        telephoneProblems(message)
+    } else if (message.body === '3' && answerTwo.has(sender)){
+        internetProblems(message)
+    }
+
+    setInterval(() => {
+        greetedUsers.clear();
+        console.log("Users have been reset")
+    }, 15 * 60 * 1000)
+})
+
+// Start the client
+client.initialize();
